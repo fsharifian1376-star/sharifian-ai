064998fa0da20a0483fa86312ccca30e36d59ca0
fsharifian1376-star <fsharifian1376@gmail.com>
2026-07-20 14:43:20 +0330

    Update script.js

--- v2/script.js
+++ v2/script.js
@@ -1,1 +1,68 @@
+// Scroll Reveal Animation
 
+const reveals = document.querySelectorAll(".reveal");
+
+
+function revealOnScroll(){
+
+    const windowHeight = window.innerHeight;
+
+
+    reveals.forEach(element => {
+
+        const elementTop = element.getBoundingClientRect().top;
+
+
+        if(elementTop < windowHeight - 100){
+
+            element.classList.add("active");
+
+        }
+
+    });
+
+}
+
+
+window.addEventListener("scroll", revealOnScroll);
+
+revealOnScroll();
+
+
+
+
+// Smooth movement for AI particles
+
+const particles = document.querySelectorAll(".particles i");
+
+
+particles.forEach((particle,index)=>{
+
+    particle.style.animationDelay = `${index * .4}s`;
+
+});
+
+
+
+
+// Small mouse interaction for AI Core
+
+const aiCore = document.querySelector(".ai-core");
+
+
+document.addEventListener("mousemove",(e)=>{
+
+
+    if(!aiCore) return;
+
+
+    const x = (window.innerWidth / 2 - e.clientX) / 80;
+
+    const y = (window.innerHeight / 2 - e.clientY) / 80;
+
+
+    aiCore.style.transform = 
+    `translate(${x}px, ${y}px)`;
+
+
+});
